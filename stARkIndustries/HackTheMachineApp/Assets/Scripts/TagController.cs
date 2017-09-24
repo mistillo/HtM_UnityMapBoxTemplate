a517a0de1a4b6c2ebfda2bf7206c0c2c2349d3a0
Todd Nelling <tnelling@gmail.com>
2017-09-24 12:12:22 -0400

    Tag Controller

---
+++ stARkIndustries/HackTheMachineApp/Assets/Scripts/TagController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TagController : MonoBehaviour
+    {
+        [Tooltip("Key to turn tag on")]
+        public KeyCode SpawnKey = KeyCode.Return;
+
+        [Tooltip("Key to turn tag off")]
+        public KeyCode DespawnKey = KeyCode.Backspace;
+
+        [Tooltip("Tags to control")]
+        public List<GameObject> Targets;
+
+        public void Update()
+        {
+            if (Input.GetKeyUp(DespawnKey))
+            {
+                Targets.ForEach(target => target.SetActive(false));
+            }
+            if (Input.GetKeyUp(SpawnKey))
+            {
+                Targets.ForEach(target => target.SetActive(true));
+            }
+        }
+    }
+}
