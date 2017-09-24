dad101b4dd1720c5884471d4dad4074e38ae40a4
greg <gregbusillodev@gmail.com>
2017-09-24 10:33:24 -0400

    old responders path

---
+++ stARkIndustries/HackTheMachineApp/Assets/Scripts/TextureAnimate.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimate : MonoBehaviour
+{
+    float xOffset=0f;
+    float yOffset=0f;
+
+    float framesPerSecond = 5.0f;
+
+    float currentTime = 0;
+
+    Vector2 offset;
+
+    private Renderer r=null;
+
+    private void Start()
+    {
+        offset = new Vector2(xOffset, yOffset);
+    }
+
+    void Update()
+    {
+        
+        if(Time.time>currentTime+.13f)
+        {
+            if(r==null)
+            {
+                try
+                {
+                    r = GetComponent<Renderer>();
+                }
+                catch { }
+            }
+            else
+            { 
+                offset.x = offset.x - .05f;
+                r.material.SetTextureOffset("_MainTex", offset);
+                currentTime = Time.time;
+            }
+ 
+        }
+
+    }
+}
